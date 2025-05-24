10a0ddb8561981ac6072007d40f6d9ebc94e59e0
pastoreekahk96 <apastoreekahk96@gmail.com>
2025-05-24 14:39:17 +0000

    Added simple_interest python file that calculate simple interest.

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+# The code calculate and prints the simple interest rate over 3 years.
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
