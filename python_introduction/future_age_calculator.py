0fccabe179292e79ced8afdd26555e146c2cb456
pastoreekahk96 <apastoreekahk96@gmail.com>
2025-05-24 17:15:04 +0000

    Added file: future_age_calculator that calculates the future age of an individual base on their current age.

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+# Calculates the future age of an individual based on their current age.
+
+current_age = int(input("How old are you? "))
+future_age = current_age + 27
+
+print(f"In 2050, you will be {future_age} years old.") 
