eacd5dc4bde9860b1c7f4edcbe40919aaead36d7
pastoreekahk96 <apastoreekahk96@gmail.com>
2025-06-01 21:12:36 +0000

    added multiplication tablem file, that calculate the users input as that multiplication table.

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -4,4 +4,4 @@ number = input("Enter a number to see its multiplication table: ")
 
 for i in range(1, 11):
     product = int(number) * i
-    print(f"{number} x {i} = {product}")
+    print(f"{number} * {i} = {product}")
