4bb3931ded005d9128f90d5ae559eb152a951fd0
pastoreekahk96 <apastoreekahk96@gmail.com>
2025-06-08 14:42:38 +0000

    Add arithmetic opperation function that calculate base on the opperation chosen by the user

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,22 @@
+"""
+Create a Python script named arithmetic_operations.py. In this script, define a function that performs basic arithmetic operations. This function, perform_operation, will be imported and used in a separate main.py script
+
+"""
+def perform_operation(num1, num2, opperation):
+    if opperation == 'add':
+        return num1 + num2
+
+    elif opperation == 'subtract':
+        return num1 - num2
+
+    elif opperation == 'multiply':
+        return num1 * num2
+
+    elif opperation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero!"
+        else:
+            return num1 / num2
+
+    else:
+        return "Input error, Invalid opperation"
