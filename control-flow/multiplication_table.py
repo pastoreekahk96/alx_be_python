e69aeb1b3bc858883d13470dfaee0c3fa14c2a4e
pastoreekahk96 <apastoreekahk96@gmail.com>
2025-06-30 14:39:41 +0000

    Deleted Intro to DB

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,7 +1,7 @@
 # This script will ask the user to enter a number, then use a for loop to print the multiplication table for that number from 1 to 10.
 
-number = input("Enter a number to see its multiplication table: ")
+number = input("Enter a number to see its multiplication table:")
 
 for i in range(1, 11):
     product = int(number) * i
-    print(f"{number} * {i} = {product}")
+    print(f"{number} x {i} = {product}")
